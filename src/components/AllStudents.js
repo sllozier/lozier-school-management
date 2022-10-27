94fa4b55186ad04c9d289f252dfae6ce61f16e2c
sllozier <sarah.lozier@gmail.com>
2022-10-27 17:40:15 -0400

    tabs working

--- src/components/AllStudents.js
+++ src/components/AllStudents.js
@@ -19,23 +19,70 @@ const AllStudents = () => {
 
 
   return (
-    <div id='students' className='column'>
+    <section className='section container is-max-desktop'>
+      <h1 className='title is-5 has-text-black'>Campus List</h1>
+      <div className='box' id="campuses">
+    <div className='list'>
       {students
         ? students.map((student) => (
-            <div className='students' key={`All Students ${student.id}`}>
+            <div className='list-item' key={`${student.id}`}>
               <Link to={`/students/${student.id}`}>
-                <h3>Name: {student.firstName} {student.lastName}</h3>
-                <h6>Email: {student.email} GPA: {student.gpa}</h6>
-                <img src={student.imageUrl}/>
+                <div className='list-item-image'>
+                  <figure className='image is-128x128'>
+                    <img className='is-rounded' src={student.imageUrl}/>
+                  </figure>
+                </div>
               </Link>
-              <button onClick={() => dispatch(deleteThisStudent(student.id))}className='delete-button'>X</button>
-              <hr/>
+              <div className='list-item-content'>
+                <div className='list-item-title'>{student.firstName} {student.lastName}</div>
+                <div className='list-item-description'>Email: {student.email} GPA: {student.gpa}</div>
+              </div>
+              <div className='list-item-controls has-text-black'>
+                <div className='buttons is-right'>
+                  <button className='button is-success' onClick={() => dispatch(deleteThisStudent(student.id))}>
+                    <span className='icon is-small'>
+                    <i className="fa-solid fa-trash-can"></i>
+                    </span>
+                    <span>Delete</span>
+                  </button>
+                </div>
+              </div>
             </div>
+            
           ))
       :null}
       <hr />
-        <AddStudent />
+      <div className='add'>
+      <AddStudent />
+      </div>
+        
     </div>
+    </div>
+    </section>
+
+
+
+
+
+
+
+    // <div id='students' className='column'>
+    //   {students
+    //     ? students.map((student) => (
+    //         <div className='students' key={`All Students ${student.id}`}>
+    //           <Link to={`/students/${student.id}`}>
+    //             <h3>Name: {student.firstName} {student.lastName}</h3>
+    //             <h6>Email: {student.email} GPA: {student.gpa}</h6>
+    //             <img src={student.imageUrl}/>
+    //           </Link>
+    //           <button onClick={() => dispatch(deleteThisStudent(student.id))}className='delete-button'>X</button>
+    //           <hr/>
+    //         </div>
+    //       ))
+    //   :null}
+    //   <hr />
+    //     <AddStudent />
+    // </div>
    )
 };
 
--- src/components/InfoTabs.js
+++ src/components/InfoTabs.js
@@ -1,37 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import AllCampuses from "./AllCampuses";
+import AllStudents from "./AllStudents";
+// import TabContent from "./TabContent";
+// import TabNavItem from "./TabNavItem";
 
 const InfoTabs = () => {
 
+    const openTab = (evt, tabName) => {
+        var i, x, tablinks;
+        x = document.getElementsByClassName("content-tab");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tab");
+        for (i = 0; i < x.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " is-active";
+      }
+    
+    
 
     return(
-        <div class="tabs is-centered">
-            <ul>
-                <li class="is-active">
+        <>
+        <section className="hero is-primary">
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                    <h1 className="title">
+                        Campus and Student Data
+                    </h1>
+                </div>
+                </div>
+
+
+
+                <div className="hero-foot">
+        
+            <nav className="tabs is-centered is-boxed is-large" >
+                <div className="container">           
+                <ul>
+                    <li className="tab is-active" onClick={() => openTab(event, "campuses")}>
                     <a>
-                    <span class="icon is-small"><i class="fas fa-image" aria-hidden="true"></i></span>
-                    <span>Pictures</span>
+                    <span className="icon is-small"><i className="fas fa-image" ></i></span>
+                    <span>Campuses</span>
                     </a>
-                </li>
-                <li>
+                    </li>
+                    <li className="tab" onClick={() => openTab(event, "students")}>
                     <a>
-                    <span class="icon is-small"><i class="fas fa-music" aria-hidden="true"></i></span>
-                    <span>Music</span>
+                    <span className="icon is-small"><i className="fas fa-music"></i></span>
+                    <span>Students</span>
                     </a>
-                </li>
-                <li>
-                    <a>
-                    <span class="icon is-small"><i class="fas fa-film" aria-hidden="true"></i></span>
-                    <span>Videos</span>
-                    </a>
-                </li>
-                <li>
-                    <a>
-                    <span class="icon is-small"><i class="far fa-file-alt" aria-hidden="true"></i></span>
-                    <span>Documents</span>
-                    </a>
-                </li>
-            </ul>
+                    </li>
+                </ul>  
+                </div>          
+        </nav>
         </div>
+        </section>
+
+        <div className="container section">
+            <div id="campuses" className="content-tab">
+                <AllCampuses/>
+            </div>
+            <div id="students" className="content-tab" >
+                <AllStudents/>
+            </div>
+        </div>
+        </>
     )
 }
 
